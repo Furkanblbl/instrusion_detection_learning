ddf024769e23fd0cc99ea33a9f7f57761d032b79
muhammet.emir <muhammet.emir@enetki.com>
2025-01-01 22:08:46 +0300

    Refactor send class

--- src/send.py
+++ src/send.py
@@ -1,54 +1,118 @@
-import pandas as pd
-import socket
+import sys
 import json
+import socket
+import argparse
+import pandas as pd
+from pprint import pprint
 
-def send_packet(packet, server_ip='127.0.0.1', server_port=8888):
-    """Sunucuya bir paket gönderir."""
-    try:
-        # TCP istemci soketi oluştur
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client_socket.connect((server_ip, server_port))
+from utils import Utils
+from dataset import Dataset
 
-        # Paketi JSON formatına çevir ve sunucuya gönder
-        packet_json = json.dumps(packet)
-        client_socket.send(packet_json.encode('utf-8'))
+class SendPacket:
+    def __init__(
+        self,
+        address: str = "127.0.0.1",
+        port: str = 8888,
+    ):
+        self.address = address
+        self.port = port
 
-    except Exception as e:
-        print(f"Hata: {e}")
-    finally:
-        client_socket.close()
+    def send_packet(self, packet):
+        """
+        Send packet to server.
+        """
+        try:
+            # Create TCP client
+            client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client_socket.connect((self.address, self.port))
+
+            # Convert json format and send to server
+            packet_json = json.dumps(packet)
+            client_socket.send(packet_json.encode("utf-8"))
+
+        # Catch errors
+        except Exception as e:
+            print(f"Error in send_packet:\n{e}")
+        finally:
+            # Finally close the socket
+            client_socket.close()
 
 
 if __name__ == "__main__":
-    # Gönderilecek örnek paket
-    file_path_full_training_set = '../nsl-kdd/KDDTrain+.txt'
-    file_path_test = '../nsl-kdd/KDDTest+.txt'
+    try:
+        parser = argparse.ArgumentParser(
+            description="Argument parser for sending data."
+        )
 
+        parser.add_argument(
+            "--sending_cols",
+            nargs="+",
+            type=str,
+            required=True,
+            help="List of columns to send (e.g., --sending_cols col1 col2 col3).",
+        )
+        parser.add_argument(
+            "--send_count",
+            type=int,
+            default=10000,
+            help="Number of items to send (default: 10000).",
+        )
+        parser.add_argument(
+            "--address",
+            type=str,
+            default="127.0.0.1",
+            help="Address to send data to (default: 127.0.0.1).",
+        )
+        parser.add_argument(
+            "--port",
+            type=int,
+            default=8888,
+            help="Port to send data to (default: 8888).",
+        )
+        parser.add_argument(
+            "--debug",
+            type=str,
+            default="True",
+            help="Debug mode, only True or False. (default: True)",
+        )
 
-    #df = pd.read_csv(file_path_20_percent)
-    test_df = pd.read_csv(file_path_test)
+        # Take all args
+        args = parser.parse_args()
 
+    # If catch any error, exit the program
+    except Exception as e:
+        print(f"Error in parser:\n{e}")
+        sys.exit(0)
 
-    # add the column labels
-    columns = (['duration' ,'protocol_type' ,'service' ,'flag' ,'src_bytes' ,'dst_bytes' ,'land' ,'wrong_fragment' ,'urgent' ,'hot' ,'num_failed_logins' ,'logged_in'
-    ,'num_compromised' ,'root_shell' ,'su_attempted' ,'num_root' ,'num_file_creations' ,'num_shells' ,'num_access_files' ,'num_outbound_cmds' ,'is_host_login'
-    ,'is_guest_login' ,'count' ,'srv_count' ,'serror_rate' ,'srv_serror_rate' ,'rerror_rate' ,'srv_rerror_rate' ,'same_srv_rate' ,'diff_srv_rate'
-    ,'srv_diff_host_rate' ,'dst_host_count' ,'dst_host_srv_count' ,'dst_host_same_srv_rate' ,'dst_host_diff_srv_rate' ,'dst_host_same_src_port_rate'
-    ,'dst_host_srv_diff_host_rate' ,'dst_host_serror_rate' ,'dst_host_srv_serror_rate' ,'dst_host_rerror_rate' ,'dst_host_srv_rerror_rate'
-    ,'attack' ,'level'])
+    # Initialize client class
+    client = SendPacket(
+        address=args.address,
+        port=args.port,
+    )
 
-    test_df.columns = columns
+    # Init Utils class
+    _utils = Utils()
+    # Load configuration from json
+    config_data = _utils.load_json_file(json_path="config.json")
 
-    for i in range(10000):
-        d  = test_df["duration"].iloc[i]
-        sb = test_df["src_bytes"].iloc[i]
-        db = test_df["dst_bytes"].iloc[i]
-        print(i, ": ", test_df["attack"].iloc[i], d, sb, db)
-        packet = {
-            "duration":  int(d),        # Bağlantı süresi (örnek değer)
-            "src_bytes": int(sb),     # Kaynaktan gönderilen bayt sayısı
-            "dst_bytes": int(db),      # Hedefe gönderilen bayt sayısı
-        }
+    # Get dataset test path
+    path_dataset_test = _utils.get_data_from_json("paths.dataset_test", config_data)
+    # Get column names path
+    path_column_names = _utils.get_data_from_json("paths.columns", config_data)
 
-        # Sunucuya paketi gönder
-        send_packet(packet)
+    # Read dataset
+    read_dataset = Dataset()
+    dataset = read_dataset.prepare_dataset(
+        path_dataset_test, col_name_path=path_column_names
+    )
+    print(args.sending_cols)
+    for i in range(args.send_count):
+        packet = {}
+        for col in args.sending_cols:
+            packet[str(col)] = int(dataset[col].iloc[i])
+
+        if args.debug == "True":
+            print(f"Sending Packet {i}: {packet}")
+
+        # Send packet to sever
+        client.send_packet(packet)
